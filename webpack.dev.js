e975eeb4dabd248c6237160d00ab3859fd14d595
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-01-17 19:01:40 +0000

    update babel to v7 and webpack deps to latest minor.patch

--- webpack.dev.js
+++ webpack.dev.js
@@ -26,7 +26,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['env']
+          presets: ['@babel/preset-env']
         }
       },
       {
--- webpack.prod.js
+++ webpack.prod.js
@@ -36,7 +36,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['env']
+          presets: ['@babel/preset-env']
         }
       },
       {
