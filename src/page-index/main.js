b556af99915dd344b9f637a424e29e9dc48c3571
ivarprudnikov <ivar.prudnikov@gmail.com>
2019-01-17 19:14:41 +0000

    add example of class transpilation on index page

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -5,3 +5,17 @@ require('./page.css');
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded", "page-index")
 });
+
+class Foo {
+  constructor(){
+    this.value = "foobar"
+  }
+  static instance() {
+    return new Foo()
+  }
+  getValue() {
+    return this.value
+  }
+}
+
+console.log(Foo.instance().getValue())
